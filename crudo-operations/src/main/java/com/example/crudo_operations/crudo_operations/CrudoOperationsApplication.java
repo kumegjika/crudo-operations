f132971cb56344c154fa07c0e3a192f5cd49c1a3
kume <kumegjika@hotmail.com>
2025-07-13 12:06:46 +0200

    Initial commit

---
+++ crudo-operations/src/main/java/com/example/crudo_operations/crudo_operations/CrudoOperationsApplication.java
@@ -0,0 +1,13 @@
+package com.example.crudo_operations.crudo_operations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudoOperationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudoOperationsApplication.class, args);
+	}
+
+}
---
+++ crudo-operations/src/test/java/com/example/crudo_operations/crudo_operations/CrudoOperationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crudo_operations.crudo_operations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudoOperationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
